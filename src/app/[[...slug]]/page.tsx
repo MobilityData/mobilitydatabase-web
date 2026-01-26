f1e117cf8ebfe38c2532e8e49481c4f8a13d180f
Alessandro Kreslin <kalessandro14@gmail.com>
2026-01-26 09:43:14 -0500

    page cleanup

--- src/app/[[...slug]]/page.tsx
+++ src/app/[[...slug]]/page.tsx
@@ -4,20 +4,9 @@
 // It will be deprecated once the migration is fully complete
 import { type ReactNode } from 'react';
 import dynamic from 'next/dynamic';
-import { useEffect, useState } from 'react';
 
 const App = dynamic(async () => await import('../App'), { ssr: false });
 
 export default function Page(): ReactNode {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return null;
-  }
-
   return <App />;
 }
