454af9e2a4df82c58c190b61a3f775d98ecb98d0
Alessandro Kreslin <kalessandro14@gmail.com>
2026-01-29 09:49:31 -0500

    lighthouse url preview var updated

--- .github/lighthouserc.js
+++ .github/lighthouserc.js
@@ -1,17 +1,5 @@
-let previewBaseUrl = '';
-try {
-  const previewUrlBase64 = process.env.LHCI_PREVIEW_URL_BASE64;
-  console.log("environemtnt variable LHCI_PREVIEW_URL:", previewUrlBase64);
-  if (previewUrlBase64) {
-    const decodedUrl = Buffer.from(previewUrlBase64, 'base64').toString('utf-8');
-    console.log("Decoded preview URL:", decodedUrl);
-    previewBaseUrl = decodedUrl;
-  } else {
-    console.error("LHCI_PREVIEW_URL_BASE64 environment variable is not set.");
-  }
-} catch (error) {
-  console.error("Error decoding LHCI_PREVIEW_URL_BASE64:", error);
-}
+let previewBaseUrl = process.env.LHCI_PREVIEW_URL || 'http://localhost:3000';
+console.log("Initial preview URL:", previewBaseUrl);
 
 module.exports = {
   ci: {
