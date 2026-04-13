3f0296c8556d117b6ae055c0b0d41471631faa7b
Alessandro Kreslin <kalessandro14@gmail.com>
2026-04-13 14:05:26 -0400

    migrated Account page

---
+++ src/app/[locale]/account/Account.styles.ts
@@ -0,0 +1,36 @@
+import { type SxProps, type Theme } from '@mui/material/styles';
+
+export const codeBlockSx: SxProps<Theme> = {
+  fontFamily: 'monospace',
+  backgroundColor: '#121c2d',
+  color: '#d7deea',
+  p: '20px',
+  borderRadius: '5px',
+  mt: '20px',
+  wordWrap: 'break-word',
+};
+
+export const codeBlockContentSx: SxProps<Theme> = {
+  fontFamily: 'monospace',
+  wordWrap: 'break-word',
+  width: '60vw',
+};
+
+export const tokenDisplayElementSx: SxProps<Theme> = (theme) => ({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  m: '5px',
+  px: '15px',
+  wordWrap: 'break-word',
+  width: '100%',
+  maxWidth: '610px',
+  backgroundColor: theme.palette.background.paper,
+  p: 2,
+  borderRadius: '6px',
+  border: `1px solid ${theme.palette.primary.main}`,
+});
+
+export const tokenActionButtonsSx: SxProps<Theme> = {
+  alignSelf: 'baseline',
+};
